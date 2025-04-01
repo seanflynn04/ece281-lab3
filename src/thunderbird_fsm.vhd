50aa20d88505f8ecf211a7d60fb7034def3bcf47
seanflynn04 <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2025-04-01 17:41:23 -0600

    thunderbird vsm update

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -36,18 +36,18 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 Binary State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 000
+--|                  ON    | 111
+--|                  R1    | 001
+--|                  R2    | 010
+--|                  R3    | 011
+--|                  L1    | 100
+--|                  L2    | 101
+--|                  L3    | 110
 --|                 --------------------
 --|
 --|
@@ -86,23 +86,68 @@ library ieee;
   use ieee.numeric_std.all;
  
 entity thunderbird_fsm is 
---  port(
-	
---  );
+    port(
+	   i_clk, i_reset  : in    std_logic;
+       i_left, i_right : in    std_logic;
+       o_lights_L      : out   std_logic_vector(2 downto 0);
+       o_lights_R      : out   std_logic_vector(2 downto 0)
+     );
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
 -- CONSTANTS ------------------------------------------------------------------
+  signal f_s : std_logic_vector (2 downto 0) := "000";
+  signal f_s_next : std_logic_vector (2 downto 0) := "000";
   
 begin
 
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
-	
+	f_s_next(2) <= (not(f_s(2)) and not(f_s(1)) and not(f_s(0)) and i_left and not(i_right)) or
+	            (not(f_s(2)) and not(f_s(1)) and not(f_s(0)) and i_left and i_right) or
+	            (f_s(2) and f_s(1) and f_s(0)) or
+	            (f_s(2) and not(f_s(1)) and not(f_s(0))) or
+	            (f_s(2) and not(f_s(1)) and f_s(0));
+	            
+	f_s_next(1) <= (not(f_s(2)) and not(f_s(1)) and not(f_s(0)) and i_left and i_right) or 
+	               (f_s(2) and f_s(1) and f_s(0)) or 
+	               (not(f_s(2)) and not(f_s(1)) and f_s(0)) or
+	               (not(f_s(2)) and f_s(1) and not(f_s(0))) or
+	               (f_s(2) and not(f_s(1)) and f_s(0));
+	               
+	f_s_next(0) <= (not(f_s(2)) and not(f_s(1)) and not(f_s(0)) and not(i_left) and i_right) or  
+	               (not(f_s(2)) and not(f_s(1)) and not(f_s(0)) and i_left and i_right) or 
+	               (f_s(2) and f_s(1) and f_s(0)) or 
+	               (not(f_s(2)) and f_s(1) and not(f_s(0))) or 
+	               (f_s(2) and not(f_s(1)) and not(f_s(0)));           
+	               
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
-    
+   
+    o_lights_L(2) <= (f_s(2) and f_s(1) and f_s(0)) or 
+                     (f_s(2) and f_s(1) and not(f_s(0)));
+                     
+    o_lights_L(1) <= (f_s(2) and f_s(1) and f_s(0)) or 
+                     (f_s(2) and not(f_s(1)) and f_s(0)) or 
+                     (f_s(2) and f_s(1) and not(f_s(0)));
+                     
+    o_lights_L(0) <= (f_s(2) and f_s(1) and f_s(0)) or 
+                     (f_s(2) and not(f_s(1)) and not(f_s(0))) or 
+                     (f_s(2) and not(f_s(1)) and f_s(0)) or 
+                     (f_s(2) and f_s(1) and not(f_s(0)));
+                     
+    o_lights_R(0) <= (f_s(2) and f_s(1) and f_s(0)) or
+                     (not(f_s(2)) and not(f_s(1)) and f_s(0)) or
+                     (not(f_s(2)) and f_s(1) and not(f_s(0))) or 
+                     (not(f_s(2)) and f_s(1) and f_s(0));
+                     
+    o_lights_R(1) <= (f_s(2) and f_s(1) and f_s(0)) or 
+                     (not(f_s(2)) and f_s(1) and not(f_s(0))) or
+                     (not(f_s(2)) and f_s(1) and f_s(0));
+                     
+    o_lights_R(0) <= (f_s(2) and f_s(1) and f_s(0)) or 
+                     (not(f_s(2)) and f_s(1) and f_s(0));                                   
 	-----------------------------------------------------					   
 				  
 end thunderbird_fsm_arch;
